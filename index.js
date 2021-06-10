8ba9f0cffaf4f939bf67b4d0aeacc46feb6fce82
jotabraga <jpmbraga.feg@gmail.com>
2021-06-09 23:47:34 -0300

    Send posts to show post page

--- index.js
+++ index.js
@@ -11,10 +11,23 @@ const posts = [{
     contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
     content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
     commentCount: 2
+  },
+  {
+    id: 2,
+    title: 'Hello World',
+    coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
+    contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
+    content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
+    commentCount: 2
   }];
 
 app.get('/',(req,res) => {
     res.send(posts);
+});
+
+app.get('/posts/:id',(req,res) => {
+    const postId = req.params.id;
+    res.send(posts.find((post) => post.id == postId));
 });
 
 app.listen(4001);
