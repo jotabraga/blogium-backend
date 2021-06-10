12534c1a418887d5a4381a9068b01f934a8df728
jotabraga <jpmbraga.feg@gmail.com>
2021-06-09 23:13:22 -0300

    Standart based on request from front-end

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 
-const posts = [];
+const posts = [{
+    id: 1,
+    title: 'Hello World',
+    coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
+    contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
+    content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
+    commentCount: 2
+  }];
 
 app.get('/',(req,res) => {
     const post = req.body;
