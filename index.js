c4c34460c26c351e3e09686283005cecd398bbf2
jotabraga <jpmbraga.feg@gmail.com>
2021-06-10 01:26:02 -0300

    Get comments by server

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const posts = [{
     id: 1,
@@ -19,7 +20,37 @@ const posts = [{
     contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
     content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
     commentCount: 2
-  }];
+  }];  
+
+  const comments = [
+    {
+        postId: 1,
+        comments: [
+            {
+                author: "João",
+                content: "Muito bom esse post! Tá de parabéns"
+            },
+            {
+                author: "Maria",
+                content: "Como faz pra dar palmas?"
+            }
+        ]
+    },
+    {
+        postId: 2,
+        comments: [
+            {
+                author: "João",
+                content: "Muito bom esse post! Tá de parabéns 2"
+            },
+            {
+                author: "Maria",
+                content: "Como faz pra dar palmas? 2"
+            }
+        ]
+    },
+];
+
 
 app.get('/',(req,res) => {
     res.send(posts);
@@ -31,9 +62,15 @@ app.get('/posts/:id',(req,res) => {
 });
 
 app.post('/new-story',(req,res) =>{
-    const newId = (posts[length-1].id + 1);
-    posts.push([...req.body, "id":newId]);
+    const newId = (posts[posts.length-1].id + 1);
+    console.log(req);
+    posts.push({...req.body, "id":newId});
     res.send();
 })
+
+app.get('/posts/:id/comments',(req,res) => {
+    const postId = req.params.id;
+    res.send(comments.find((comment) => comment.postId == postId));
+});
 
 app.listen(4001);
